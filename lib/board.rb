e54f0e0cbffc6ecbd9bccb02910a1829982123d5
Jeff Stringer & Michael Derfler <student@epicodus.com>
2014-08-01 11:04:44 -0700

    update Board class

---
+++ lib/board.rb
@@ -0,0 +1,16 @@
+class Board
+
+  attr_accessor :board_spaces
+
+  def initialize
+    @players_spaces = []
+    @board_spaces = []
+    #create 9 spaces and push into board_spaces
+    i = 0
+    9.times do |i|
+      @board_spaces << Space.new(i, '')
+      i+=1
+    end
+  end
+end
+
--- spec/tic_spec.rb
+++ spec/tic_spec.rb
@@ -13,7 +13,14 @@ end
 
 describe Space do
   it 'is initialized by a space' do
-    test_space = Space.new('4')
+    test_space = Space.new('4', 'X')
     expect(test_space).to be_an_instance_of Space
+  end
+end
+
+describe Board do
+  it 'creates nine spaces when initialized' do
+    test_board = Board.new
+    expect(test_board.board_spaces.length).to eq 9
   end
 end
